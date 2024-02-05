2cdda0982d1141313b3bf2e205d578532e5c7193
fxbrb <f.barbier96@gmail.com>
2024-02-05 17:40:17 +0100

    resend api key fix

--- src/app/api/forgot-password/route.ts
+++ src/app/api/forgot-password/route.ts
@@ -1,9 +1,8 @@
 import { prisma } from "@/lib/db";
 import { generateToken } from "@/lib/generateToken";
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
